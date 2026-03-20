45e05cf777d270e2ee77923d225facd9a5c973ee
samuel922 <onyangosamuel583@gmail.com>
2026-03-20 10:03:39 +0300

    completed fetching and rendering

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
+let colorObj = {}
+
+document.getElementById("get-color-scheme").addEventListener('click', fetchColorScheme)
 
 
-fetch("https://www.thecolorapi.com/scheme?hex=0047AB&format=json&mode=monochrome&count=5", {
+
+function fetchColorScheme() {
+
+    const selectColorVal = document.getElementById("select-color").value
+
+
+    fetch(`https://www.thecolorapi.com/scheme?hex=0047AB&format=json&mode=${selectColorVal}&count=5`, {
     method: "GET"
-})
-    .then(res => res.json())
-    .then(data => {
-        console.log(typeof data)
     })
+        .then(res => res.json())
+        .then(data => {
+            renderColorSchemes(data)
+            
+        })
+
+}
+
+
+
+function renderColorSchemes(data) {
+
+    const colorColEl = data.colors.map((color) => {
+        return `
+            <div class="color-item">
+                <div class="color" style="background-color: ${color.hex.value};"></div>
+                <div class="text-cont">
+                    <p>${color.hex.value}</p>
+                </div>
+            </div>
+        
+        `
+    }).join("")
+
+    // console.log(mainColor)
+    document.getElementById('head-color-box').style.backgroundColor = `${data.colors[1].hex.value}`
+    document.getElementById("color-grid-container").innerHTML = colorColEl
+
+}
